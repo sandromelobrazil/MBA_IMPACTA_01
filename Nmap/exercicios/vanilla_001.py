ab8cfc2d79e2fb077be12e6370722fbf438c2d40
yellow <yellow@battosai.yellow.dojo>
2021-09-15 22:41:41 -0300

    Aula NMAP / Python - 15/09/2021- 22h41

---
+++ Nmap/exercicios/vanilla_001.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3.8
+import nmap3
+import json
+
+pynmap = nmap3.NmapScanTechniques()
+
+target = '11.11.11.171'
+
+vanilla = pynmap.nmap_tcp_scan(target)
+vanilla_json = json.dumps(vanilla, indent = 6)
+
+print(vanilla_json)
